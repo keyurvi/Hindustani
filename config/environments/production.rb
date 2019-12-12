4003ceccc27745494be738735096fd02e04e3081
nitinb-c-metric <nitin.barai@c-metric.com>
2019-12-12 13:10:39 +0530

    locale changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: ActionMailer::Base.default_url_options[:host] }
   config.action_mailer.smtp_settings = {
     address:       'smtp.gmail.com',
     port:          587,
