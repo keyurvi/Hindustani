e0b2dfac1148846571c82a87bec9ad34d49262a3
nitinb-c-metric <nitin.barai@c-metric.com>
2019-12-12 13:12:21 +0530

    locale changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ActionMailer::Base.default_url_options[:host] }
+  config.action_mailer.default_url_options = { host: 'aqueous-ocean-96074.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:       'smtp.gmail.com',
     port:          587,
