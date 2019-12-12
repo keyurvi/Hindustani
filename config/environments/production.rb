76298e2762db4eee4b412ff15fc1af38d48da095
nitinb-c-metric <nitin.barai@c-metric.com>
2019-12-12 12:58:08 +0530

    locale changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
     port:          587,
     domain:         'gmail.com',
     user_name:      'keyurvi.dev@gmail.com',
-    password:       'K@p280897',
+    password:       'ypwhikwbwvndhtat',
     authentication: :plain,
     enable_starttls_auto: true
   }
